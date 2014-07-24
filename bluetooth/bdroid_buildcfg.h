06e83c8dc7f33985b9b7b77f0e8213de73a3a311
artas182x <artas182x@gmail.com>
2014-07-24 19:13:43 +0200

    Merge my changes into git

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,6 +17,10 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "LG-P760"
+#define BTM_DEF_LOCAL_NAME   "LG Optimus L9"
+
+#define BLE_INCLUDED TRUE
+#define BTA_GATT_INCLUDED TRUE
+#define SMP_INCLUDED TRUE
 
 #endif
--- device.mk
+++ device.mk
@@ -32,10 +32,13 @@ PRODUCT_COPY_FILES += \
 
 # Permission files
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+	frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+	frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
 
 # NFC packages
 PRODUCT_PACKAGES += \
+	com.android.nfc_extras \
     libnfc \
     libnfc_jni \
     Nfc \
